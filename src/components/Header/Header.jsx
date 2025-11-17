c3efcb4a2bdff1759e7c20eb6cea6536e79e3ccd
alvarocsr2 <alvarocesarmm@gmail.com>
2025-11-17 16:19:41 -0300

    init:Iniciei o projeto com a criação do componente 'Header' e sua estilização com o SASS.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import "./Header.scss/"
+import Logomarca from "../../assets/Imgs/Logomarca.png"
+
+function Header(){
+  return (
+   <header>
+    <img src={Logomarca} alt="Foto da logo do site" />
+
+    <ul>
+    <li>LOOKS</li>
+    <li>LANÇAMENTOS</li>
+    <li>NOVIDADES</li>
+    
+    </ul>
+
+
+
+
+
+   </header>
+  )
+}
+
+export default Header
